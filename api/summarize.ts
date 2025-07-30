b916f799b36b0d1ed7d466236cf1133fee50465d
Elebesunu Osereme <elebesunuosereme@gmail.com>
2025-07-30 10:02:24 +0100

    still testing

--- api/summarize.ts
+++ api/summarize.ts
@@ -135,12 +135,7 @@
 //   });
 // }
 
-export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).end("Method Not Allowed");
-  }
-  return res
-    .status(200)
-    .json({ message: "Summarization API is under construction" });
-  // rest of code...
+function handler(req, res) {
+  res.status(200).json({ message: "Testing summarize api" });
 }
+export default handler;
